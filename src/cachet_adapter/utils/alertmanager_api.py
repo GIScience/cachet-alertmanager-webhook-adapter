eac37ed4e1997ac90cb06215740757f091f07cba
Moritz Schott <moritz.schott@heigit.org>
2026-07-02 13:23:02 +0000

    feat(sync-alerts): add script to docker

--- src/cachet_adapter/utils/alertmanager_api.py
+++ src/cachet_adapter/utils/alertmanager_api.py
@@ -2,7 +2,7 @@ from cachet_adapter.utils.http_connection import HttpConnection
 
 
 class AlertmanagerApi(HttpConnection):
-    def get_alerts(self) -> dict:
+    def get_alerts(self) -> list[dict]:
         response = self.session.get(f'{self.base_url}/alerts')
         response.raise_for_status()
         alerts = response.json()
